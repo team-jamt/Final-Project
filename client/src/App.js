6c776d2386468574cc730ef1b4214ce7fe1781e4
Jonas Davidow <jonas.davidow@gmail.com>
2019-02-15 17:55:11 -0600

    sign up page

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,32 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
+import Signup from "./components/sign-up";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h2>Welcome to React</h2>
+//         </div>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Signup />
       </div>
-    );
-  }
+    </Router>
+  )
 }
-
 export default App;
