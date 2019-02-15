1de21dba3aa5d40bca52662778db95cbc1dc8b65
melwell027 <melwell1027@Gmail.com>
2019-02-14 21:51:16 -0600

    adding stucture

---
+++ client/build/precache-manifest.b3c9802f354e7c5f303d38b243128cef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8de485802efc82ce4693",
+    "url": "/static/js/main.8de48580.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "8de485802efc82ce4693",
+    "url": "/static/css/main.8dec7940.chunk.css"
+  },
+  {
+    "revision": "b165d8b7c87cc0b4565b46a916d15ff0",
+    "url": "/index.html"
+  }
+];
