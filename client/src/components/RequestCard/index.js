6005b367f34bd98dae5ede0ff050126de3182e41
melwell027 <melwell1027@Gmail.com>
2019-02-23 14:59:14 -0600

    added style to requests page

--- client/src/components/RequestCard/index.js
+++ client/src/components/RequestCard/index.js
@@ -4,25 +4,31 @@ function RequestCard(props) {
     return (
         <div className="request-card">
             <div class="row">
-                <div className="col-sm-6">
-                    <h5 className="card-title">{props.name}</h5>
-                    <p className="card-text">{props.description}</p>
+                <div className="col-sm-5">
+                    <h5 className="request-title">{props.name}</h5>
+                    <p className="request-description">{props.description}</p>
+                </div>
+
+                <div className="col-sm-4">
+                    <p className="middle-col">
+                        <span class="middle-col-item">Category:</span>
+                        <span>{props.category}</span>
+                    </p>
+
+                    <p className="middle-col">
+                        <span class="middle-col-item">Will Pay:</span>
+                        <span>{props.price}</span>
+                    </p>
+
+
                 </div>
 
                 <div className="col-sm-3">
-                    <h6>{props.type}</h6>
-                    <h6>{props.category}</h6>
-                    <h6>{props.price}</h6>
-                </div>
 
-                <div className="col-sm-3">
-                    <div class="img-container">
-                        <img alt={props.name} src={props.image} />
-                    </div>
-                    <a href="https://www.google.com" classname="btn">Respond to Request</a>
+                    <button className="request-btn"> <a href="https://www.google.com" className="request-link">Respond to Request</a></button>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
