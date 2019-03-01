2f9073790c8aed1cbbe91e6f1a5c1d3298ac8cc8
melwell027 <melwell1027@Gmail.com>
2019-02-28 19:10:36 -0600

    Adding styled homepage

--- client/build/precache-manifest.41cc2301d178b0eaf702bb8e567aca92.js
+++ client/build/precache-manifest.41cc2301d178b0eaf702bb8e567aca92.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.fdfcfda2.js"
   },
   {
-    "revision": "6a8fe836c1c9359f54ed",
-    "url": "/static/js/main.6a8fe836.chunk.js"
+    "revision": "6f529d13adc6e32b4935",
+    "url": "/static/js/main.6f529d13.chunk.js"
   },
   {
     "revision": "4f21be3f5b2e38a7b3f4",
     "url": "/static/js/2.4f21be3f.chunk.js"
   },
   {
-    "revision": "6a8fe836c1c9359f54ed",
-    "url": "/static/css/main.17b68dfc.chunk.css"
+    "revision": "6f529d13adc6e32b4935",
+    "url": "/static/css/main.d6fef843.chunk.css"
   },
   {
-    "revision": "28a0e037f0645779a7d21060df51905a",
+    "revision": "54a2e6785740024eb3d54651fa8f9cf2",
     "url": "/index.html"
   }
 ];
