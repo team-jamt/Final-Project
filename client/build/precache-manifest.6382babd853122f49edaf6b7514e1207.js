86330201a9be2d443b67a8ba26bec762741d82dd
melwell027 <melwell1027@Gmail.com>
2019-03-02 02:28:16 -0600

    more styling

--- client/build/precache-manifest.6382babd853122f49edaf6b7514e1207.js
+++ client/build/precache-manifest.6382babd853122f49edaf6b7514e1207.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.fdfcfda2.js"
   },
   {
-    "revision": "896123e62eb85f04d412",
-    "url": "/static/js/main.896123e6.chunk.js"
+    "revision": "b385ce49ffd4aefe3c02",
+    "url": "/static/js/main.b385ce49.chunk.js"
   },
   {
     "revision": "9d5fe2216b2ffdeb02f7",
     "url": "/static/js/2.9d5fe221.chunk.js"
   },
   {
-    "revision": "896123e62eb85f04d412",
-    "url": "/static/css/main.caca8e4a.chunk.css"
+    "revision": "b385ce49ffd4aefe3c02",
+    "url": "/static/css/main.1b53d1d8.chunk.css"
   },
   {
-    "revision": "76225c3094663ea0473f76d7fc2c64df",
+    "revision": "10197cfe5ba942e460d39d5937f6a1e5",
     "url": "/index.html"
   }
 ];
